79a7193859043b8e5c6e2cffab5d487a5ccdb710
Sykes, Ben (GE Digital) <ben.sykes@ge.com>
2017-08-14 16:08:00 -0700

    Switch to hash-routing for ease with nginx hosting

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
 import { default as Stirling } from 'material-ui/svg-icons/hardware/toys';
 import SelectField from 'material-ui/SelectField';
 import MenuItem from 'material-ui/MenuItem';
